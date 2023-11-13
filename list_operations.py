575c2ef2670bef659924d0faed6e1c78a6d6e516
RyzenMu <muruga13in2022@gmail.com>
2023-11-14 04:46:31 +0530

    mutable and immutable objects

---
+++ list_operations.py
@@ -0,0 +1,17 @@
+
+# example 1
+list1 = ['red', 'green']
+list2 = [10, 20, 30]
+
+# multiplication operator *
+list_multiply = list2 * 2
+print(list_multiply)
+list2 = ['q', 'k', 'n', 'a', 7, 987, 334]
+list_multiply = list2 * 5
+print(list_multiply)
+
+# concatenation operator +
+list1 = ['red', 'green']
+list2 = [10, 20, 30]
+list3 = list2 + list2 + list1
+print(list3)
