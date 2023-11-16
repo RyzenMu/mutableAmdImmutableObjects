182a5b058ccaaa9c6c5c2399928f27d5fd503c10
RyzenMu <muruga13in2022@gmail.com>
2023-11-17 04:53:25 +0530

    set intersection

--- set_methods.py
+++ set_methods.py
@@ -1,4 +1,4 @@
-# min , ,max, sum, len funcyions works as same as listrs
+# min , ,max, sum, len and in funcyions works as same as listrs
 
 digits = set((3, 6, 3, 4, 7, 7, 4, 9))
 
@@ -6,3 +6,5 @@ print(min(digits))
 print(max(digits))
 print(sum(digits))
 print(len(digits))
+
+print(3 in digits)
